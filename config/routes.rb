9e02d1074c54cda6bd3d30ddd0881a9400916691
Dan Rice <dan.rice.92@outlook.com>
2016-07-30 21:38:53 +0000

    Blocipedia 3: User Sign in and out completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   get 'welcome/index'
 
   get 'welcome/about'
 
   root 'welcome#index'
+  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  
 end
