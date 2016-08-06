0d84767ca7583efb0c786790c5a79657b31bcb93
Dan Rice <dan.rice.92@outlook.com>
2016-08-06 04:14:03 +0000

    Blocipedia 6: Seeding Data completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+5.times do
+    User.create!(
+        email: RandomData.random_email,
+        password: "Arandompassword123",
+        standard: true,
+        premium: false,
+        admin: false
+        )
+end
+
+3.times do
+    User.create!(
+        email: RandomData.random_email,
+        password: "Arandompremiumpassword123",
+        standard: false,
+        premium: true,
+        admin: false
+        )
+end
+
+2.times do
+    User.create!(
+        email: RandomData.random_email,
+        password: "Arandomadminemail123",
+        standard: false,
+        premium: false,
+        admin: true
+        )
+end
+
+users = User.all
+
+100.times do
+    Wiki.create!(
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        private: false
+        )
+end
+
+wikis = Wiki.all
+
+puts "Seed finished"
+puts "#{users.count} users created"
+puts "#{wikis.count} wikis created"
