53c476b24573d03c7cac4314f08904f21f6d9f00
Dan Rice <dan.rice.92@outlook.com>
2016-09-05 19:58:21 -0600

    Update Collaborator db

---
+++ db/migrate/20160906015445_add_user_to_collaborators.rb
@@ -0,0 +1,6 @@
+class AddUserToCollaborators < ActiveRecord::Migration
+  def change
+  	add_column :collaborators, :user_id, :integer
+  	add_index :collaborators, :user_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160906015445) do
 
   create_table "collaborators", force: :cascade do |t|
     t.integer  "wiki_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+    t.integer  "user_id"
   end
+
+  add_index "collaborators", ["user_id"], name: "index_collaborators_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
